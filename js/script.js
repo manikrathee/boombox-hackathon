03659c6c5f2024700150e0059fae9d6d92c28b5f
Manik Rathee <manikrathee@gmail.com>
2012-10-14 23:18:33 -0500

    closing out the hackday(s) for the Stream View

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-/*
- * scripts.js
- *
- * auto-concatenated jquery, modernizr and custom alternate files
- *
- */
-
-//@codekit-prepend "libs/jquery-1.7.1.min.js", "libs/ss-standard.js";
+//@codekit-prepend "libs/jquery-1.7.1.min.js", "libs/ss-standard.js", "libs/jquery.tiptip.js";
 
 $.ajax({
 	"type": "GET",
@@ -20,54 +13,98 @@ $.ajax({
 		// loop through the first 12
 		var i = 0;
 		var feedObject = JSON.parse(r.responseText);
+		
 		for (i; i<12; i++) {
-		  $('#main-stream').append('<div class="stream-item"><div class="twelvecol track-info"><h3 class="track">' + feedObject.result[i].metadata.title + '</h3><h3 class="artist">' + feedObject.result[i].metadata.artist.name + '</h3></div> <!-- /twelvecol track-info --><div class="fourcol track-image"><img class="album-art" src="' + feedObject.result[i].images['128x128'].url + '" alt="' + feedObject.result[i].post.title + '"></div> <!-- /fourcol /track-info --></div> <!-- /twelvecol /stream-item -->');
+		  $('#main-stream').append('<div class="stream-item"><div class="threecol track-image"><img class="album-art" src="' + feedObject.result[i].images['128x128'].url + '"></div> <!-- /fourcol /track-info --><div class="ninecol last track-info"><h3 class="track">' + feedObject.result[i].metadata.title + '</h3><h3 class="artist">' + feedObject.result[i].metadata.artist.name + '</h3><div class="eightcol description">' + feedObject.result[i].post.description + '</div> <!-- /fourcol /track-info --><div class="fourcol last"><ul class="track-actions ss-icon"><li><a href="#" id="action-thumbs-up" class="track-action-item" title="Like This Track">thumbsup</a></li><li><a href="#" id="action-thumbs-down" class="track-action-item" title="Dislike This Track">thumbsdown</a></li><li><a href="#" class="track-action-item" title="Flag This As Interesting">flag</a></li><li><a href="#" class="track-action-item" title="Share This Track">send</a></li><li><a href="#" class="track-action-item" title="View Artist Profile">users</a></li></ul></div> <!-- /fourcol /track-info --></div> <!-- /ninecol track-info --></div><!-- /stream-item -->');
+		  $('.stream-item').data('album-art-background', feedObject.result[i].images['640x400'].url);
+		  console.log( $('.stream-item').data('album-art-background') );
 		}
-		for (i = 20;i<40; i++){
-			$('#main-stream').append('<div class="stream-item condensed"><div class="twelvecol track-info"><h3 class="track">' + feedObject.result[i].metadata.title + '</h3><h3 class="artist">' + feedObject.result[i].metadata.artist.name + '</h3></div> <!-- /twelvecol track-info --></div> <!-- /twelvecol /stream-item -->');
-		}
+		
+		// Loop through the next 20 for the condensed track list
+		// for (i = 20;i<40; i++){
+		// 	$('#main-stream').append('<div class="stream-item condensed"><div class="twelvecol track-info"><h3 class="artist">' + feedObject.result[i].metadata.artist.name + '</h3><h3 class="track">' + feedObject.result[i].metadata.title + '</h3></div> <!-- /twelvecol track-info --></div> <!-- /twelvecol /stream-item -->');
+		// }	
+		
+		$('body').addClass('active');
+		
+		// Tooltip
+		$(function(){
+			$(".track-action-item").tipTip({
+				delay: 250
+			})
+		});
 	}
 });
 
-// var title = results.response.oneforty;
-  //       var numTweets = results.response.trackback_total;
-  //       $('#results').append(title + ' has ' + numTweets + ' tweets.');
+var streamItem;
+var albumArt;
 
 
+// Shrink page header after load
+$('#page-header h1').delay('12000').addClass('loaded');
 
-// var trackInfo = $('.track-info');
 
-// trackInfo.click(function(){
-// 	$('body').css('background-color','red');
-// });
+// Now Playing & Background Album Art
+$('#main-stream').on('click', '.stream-item', function(){
+		var artist = $(this).find('.artist').text();
+		var track = $(this).find('.track').text();
+		var nowPlaying = artist + track;
+		
+		$('#now-playing-title').text( nowPlaying );
+		
+		$('#pro-tip').fadeOut(800);
+		
+		if (!$('#player').hasClass('active')){
+			$('#player').addClass('active');
+		}
+		
+		if ($('#now-playing').hasClass('hidden')){
+			$('#now-playing').removeClass('hidden');
+		}
+		
+		// albumArt = $('this').data('album-art-background');
+		// if ($('body').hasClass('active')){
+		// 	$('body').addClass('tracked').css('background-image','url("' + albumArt + '")');
+		// }
+});
 
+
+// Set ACTIONS to active/inactive on Click
+$('#main-stream').on('click', '.track-action-item', function(e){
+	e.preventDefault();
+	e.stopPropagation();
+	$(this).toggleClass('active');
+	
+	if ($(this).prop('id','action-thumbs-down')){
+		if ($('#action-thumbs-up').hasClass('active')){
+			$('#action-thumbs-up').removeClass('active');
+		}
+	}
+	if ($(this).prop('id','action-thumbs-up')){
+		if ($('#action-thumbs-down').hasClass('active')){
+			$('#action-thumbs-down').removeClass('active');
+		}
+	}
+});
 
 
 // Player Opacity Controls
  $(function() {
     $(window).scroll(function(){
-		var scrollTop = $(window).scrollTop();
-		if(scrollTop >= 20){
+		var scrollPosition = $(window).scrollTop();
+		if(scrollPosition >= 20){
 			$('#player').addClass('fixed');
+			$('#page-header h1').addClass('loaded');
 		}
-		if(scrollTop < 20){
+		if(scrollPosition < 20){
 			$('#player').removeClass('fixed');
+			$('#page-header h1').removeClass('loaded');
+		}
+		if(scrollPosition < 120){
+			$('#sidebar').addClass('fixed');
+		}
+		if(scrollPosition < 120){
+			$('#sidebar').removeClass('fixed');
 		}
 	});
-		
-	// $('#player').hover(
-	// 	function (e) {
-	// 		var scrollTop = $(window).scrollTop();
-	// 		if(scrollTop != 0){
-	// 			$('#player').stop().animate({'opacity':'1'},400);
-	// 			$('#player').stop().animate({'opacity':'1'},400);
-	// 		}
-	// 	},
-	// 	function (e) {
-	// 		var scrollTop = $(window).scrollTop();
-	// 		if(scrollTop != 0){
-	// 			$('#player').stop().animate({'opacity':'0.5'},400);
-	// 		}
-	// 	}
-	// );
 });
